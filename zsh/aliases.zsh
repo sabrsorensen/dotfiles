d165d640c2f7034dd43dedc31a42345a951c5489
Sam Sorensen <sabrsorensen@gmail.com>
2015-10-11 14:42:19 -0600

    Updated rtorrent config and added reflector alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,3 +6,4 @@ alias cp="rsync -avz"
 alias rtorrent="rtorrent -D"
 alias sudo="sudo -E"
 alias rename=perl-rename
+alias reflector="sudo reflector --verbose --save /etc/pacman.d/mirrorlist --sort age --threads 2 -a 24 -f 20 -c 'United States' -l 200"
